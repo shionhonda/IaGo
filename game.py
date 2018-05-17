62d1ada05238b73da93655c34685650ab250c3f7
shionhonda <26x.orc.ed5.1hs@gmail.com>
2018-05-17 15:00:12 +0900

    refactor

--- game.py
+++ game.py
@@ -2,12 +2,10 @@ import os
 from datetime import datetime
 import numpy as np
 import random
-
 import chainer
 import chainer.links as L
 from chainer import training, serializers, cuda, optimizers, Variable
 from chainer.functions.loss.softmax_cross_entropy import softmax_cross_entropy
-
 import SLPolicy
 
 class Game:
@@ -29,32 +27,38 @@ class Game:
         self.model = L.Classifier(SLPolicy.SLPolicyNet(), lossfun=softmax_cross_entropy)
         serializers.load_npz('model.npz', self.model)
 
-    # Place a stone and turn all the stones
+    # Return True if the index is out of the board
+    def is_outside(self, pos):
+        return pos[0]<0 or pos[0]>7 or pos[1]<0 or pos[1]>7
+
+    # Place a stone and turn all the sandwithced stones
     # Position y:vertical, x:horizontal
     # Color  1:white, 2:black
     def place_stone(self, position, color):
         # Place the stone
         pos = np.array(position)-[1,1]
         self.state[pos[0], pos[1]] = color
-        # Turn stones
+        # Search for sandwitched stones
         dys = [-1, -1, -1, 0, 0, 1, 1, 1] # Search direction
         dxs = [-1, 0, 1, -1, 1, -1, 0, 1] # Search direction
         for dy,dx in zip(dys, dxs):
-            if pos[0]+dy<0 or pos[0]+dy>7 or pos[1]+dx<0 or pos[1]+dx>7:
-                continue
+            if self.is_outside(pos+[dy,dx]):
+                continue # Search next direction if index goes out of range
             if self.state[pos[0]+dy, pos[1]+dx]+color!=3:
-                continue
-            ref = pos + [dy, dx]
+                continue # Search next direction if empty or same-color stone
+            ref = pos + [dy, dx] # Decide direction
             while(self.state[ref[0], ref[1]]+color==3):
-                ref += [dy, dx]
-                if ref[0]<0 or ref[0]>7 or ref[1]<0 or ref[1]>7:
-                    break
-            if ref[0]>=0 and ref[0]<=7 and ref[1]>=0 and ref[1]<=7:
-                if self.state[ref[0], ref[1]]==color:
+                ref += [dy, dx] # Referring index
+                if self.is_outside(ref):
+                    break # Stop if referring index goes out of range
+            if self.is_outside(ref):
+                continue # Search next direction if index goes out of range
+            # Turn sandwitched stones
+            if self.state[ref[0], ref[1]]==color:
+                ref -= [dy, dx]
+                while(self.state[ref[0], ref[1]]+color==3):
+                    self.state[ref[0], ref[1]] = 3-self.state[ref[0], ref[1]]
                     ref -= [dy, dx]
-                    while(self.state[ref[0], ref[1]]+color==3):
-                        self.state[ref[0], ref[1]] = 3-self.state[ref[0], ref[1]]
-                        ref -= [dy, dx]
 
     def valid_pos(self, color):
         positions = []
@@ -66,21 +70,23 @@ class Game:
                 dys = [-1, -1, -1, 0, 0, 1, 1, 1]
                 dxs = [-1, 0, 1, -1, 1, -1, 0, 1]
                 for dy,dx in zip(dys, dxs):
-                    if i+dy<0 or i+dy>7 or j+dx<0 or j+dx>7:
+                    if self.is_outside([i+dy, j+dx]):
                         continue
                     if self.state[i+dy, j+dx]+color!=3:
                         continue
                     ref = np.array([i+dy, j+dx])
                     while(self.state[ref[0], ref[1]]+color==3):
                         ref += [dy, dx]
-                        if ref[0]<0 or ref[0]>7 or ref[1]<0 or ref[1]>7:
+                        if self.is_outside(ref):
                             break
-                    if ref[0]>=0 and ref[0]<=7 and ref[1]>=0 and ref[1]<=7:
-                        if self.state[ref[0], ref[1]]==color:
-                            positions.append([i+1,j+1])
-                            break
+                    if self.is_outside(ref):
+                        continue
+                    if self.state[ref[0], ref[1]]==color:
+                        positions.append([i+1,j+1])
+                        break
         return positions
 
+    # Convert ndarray to a board-like string
     def show(self):
         print("   1   2   3   4   5   6   7   8   ")
         for i in range(8):
@@ -93,6 +99,7 @@ class Game:
         + ", Empty:" + str(np.sum(self.state==0)))
         print("\n")
 
+    # Judge game winner
     def judge(self):
         you = np.sum(self.state==1)
         ai = np.sum(self.state==2)
@@ -104,32 +111,39 @@ class Game:
             print("DRAW")
         return "X(You):"+ str(you) + ", O(AI):" + str(ai) + ", Empty:" + str(np.sum(self.state==0))
 
+    # Input function tolerant for mistyping
     def safeinput(self):
         line = input()
         if line.isspace():
-            return safeinput()
+            return safeinput() # Recurse this function for mistyping
         line = line.split(',')
         if len(line) != 2:
             print("Try again.")
-            return safeinput()
+            return safeinput() # Recurse this function for mistyping
         else:
             return line
 
+    # Get position to place stone
     def get_position(self, color, positions):
+        # Your turn
         if color==1:
             position = [int(e) for e in self.safeinput()]
             if not position in positions:
                 print("This position is invalid. Choose another position")
-                return self.get_position(self, color, positions)
+                return self.get_position(color, positions) # Recurse
+        # AI's turn
         else:
+            # Predict position to place stone
             state_var = chainer.Variable(self.state.reshape(1, 1, 8, 8).astype(np.float32))
             action_probabilities = self.model.predictor(state_var).data.reshape(64)
             idx = np.argmax(action_probabilities)
             position = [idx//8+1, idx%8+1]
             if not position in positions:
+                # Choose randomly if prediction is illegal (very rare)
                 position = random.choice(positions)
         return position
 
+    # Things to do in one turn
     def turn(self, color):
         players = ["You", "AI"]
         positions = self.valid_pos(color)
@@ -144,12 +158,13 @@ class Game:
             self.stone_num += 1
         else:
             if self.pass_flg:
-                self.stone_num = 64
+                self.stone_num = 64 # Game over when two players pass consecutively
             print(players[color-1] + " pass.")
             self.pass_flg = True
             self.gamelog += "[" + str(self.play_num) + "]" + players[color-1] + ": Pass\n"
         self.play_num += 1
 
+# Whole game
 def main():
     print("\n"+"*"*34)
     print("*"*11+"Game Start!!"+"*"*11)
@@ -166,9 +181,9 @@ def main():
     print("*"*34)
     jd = game.judge()
     print(jd)
-    self.gameleog += jd + "\n"
-    with open("./gamelog/"+self.date+".txt", "w") as f:
-        f.write(self.gamelog)
+    game.gamelog += jd + "\n"
+    with open("./gamelog/"+game.date+".txt", "w") as f:
+        f.write(game.gamelog)
 
 if __name__ == '__main__':
     main()
